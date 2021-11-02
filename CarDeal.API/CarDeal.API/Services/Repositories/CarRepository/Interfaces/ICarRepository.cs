823587421a937cef2903dbd8fee8c286a78b6f3b
Damian Baranowski <damian.baranowski20@gmail.com>
2021-11-02 12:49:27 +0100

    IRepository has been implemented for Get Newest and Get Filtered Ads

--- CarDeal.API/CarDeal.API/Services/Repositories/CarRepository/Interfaces/ICarRepository.cs
+++ CarDeal.API/CarDeal.API/Services/Repositories/CarRepository/Interfaces/ICarRepository.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CarDeal.API.Services.EF.Entities.Cars;
+using CarDeal.API.Services.Repositories.CarRepository.Filters;
+
 namespace CarDeal.API.Services.Repositories.CarRepository.Interfaces
 {
     public interface ICarRepository
     {
-        
+       Task<List<CarAd>> GetNewest(int numberOfRecords); 
+       
+       Task<List<CarAd>> GetFiltered(int numberOfRecords, CarFilter filterObject); 
     }
 }
